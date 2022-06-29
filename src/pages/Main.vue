6a0f14726ca348a204942d09ca33528f31bd4912
Andrey Komov <andrewkomov@gmail.com>
2022-06-30 00:12:57 +0300

    main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,8 +1,12 @@
 <template>
-  <main-header></main-header>
-  <main-sliders></main-sliders>
-  <main-content></main-content>
-  <main-footer></main-footer>
+  <body>
+  <div class="main-wrapper">
+    <main-header></main-header>
+    <main-sliders></main-sliders>
+    <main-content></main-content>
+    <main-footer></main-footer>
+  </div>
+  </body>
 </template>
 
 <script>
@@ -22,6 +26,14 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+body{
+  display: flex;
+    .main-wrapper{
+      margin: auto;
+      flex-basis: 1280px;
+      font-family: SegoeWP;
+      border: solid 1px rgba(0, 0, 1, 0.16);
+    }
+}
 </style>
